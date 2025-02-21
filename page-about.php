16c1e99c77bda88ff779d618e71015bfb1af6392
Anderson Benedetti <andersonjcbenedetti@gmail.com>
2025-02-21 15:37:44 -0300

    feat: add page about us

---
+++ page-about.php
@@ -0,0 +1,55 @@
+<?php // Template Name: Sobre nós ?>
+
+<?php get_header(); ?>
+
+<main id="pg-about">
+
+  <section class="intro section-aba">
+    <div class="container">
+      <div class="content">
+        <div>
+          <h2>Sobre nós</h2>
+          <h1>Construindo nosso futuro <span>juntos!</span></h1>
+        </div>
+        <img class="img" src="<?php echo get_stylesheet_directory_uri(); ?>/img/sobre-nos.webp" alt="Foto Sobre nós">
+        <a href="#" class="btn-section">
+          <p>Conheça a imbralit</p>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/arrow-down.svg" alt="Icon arrow">
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <section class="section-infos">
+    <div class="container">
+      <div class="texts">
+        <h2>A Imbralit ocupa um lugar de destaque entre as empresas líderes do mercado nacional.</h2>
+        <p>Fundada em 1974, com sede em Criciúma e única indústria do setor do fibrocimento no Estado de Santa
+          Catarina, a Imbralit faz parte do Grupo Jorge Zanatta, um dos mais importantes grupos empresariais
+          da
+          Região Sul do Estado.</p>
+        <p>Fabricando e comercializando telhas onduladas, peças complementares, placas planas de fibrocimento e
+          outros materiais de construção, a empresa atua em todo o Brasil e em diversos mercados no exterior.
+        </p>
+        <p>Pioneira entre as empresas do setor com capital 100% nacional a fabricar produtos de fibrocimento
+          reforçados exclusivamente com fibras sintéticas e vegetais. Pioneira nacional no setor do
+          fibrocimento a
+          conquistar as certificações ISO 9001 e 14001. Primeira empresa brasileira do setor a assegurar
+          garantia
+          de qualidade de 10 anos para os produtos de fibrocimento, e também a primeira do segmento a oferecer
+          um
+          canal de vendas online. Reconhecida como a marca de fibrocimento com presença no maior número de
+          revendas na Região Sul do Brasil, a Imbralit ocupa um lugar de destaque entre as empresas líderes do
+          mercado nacional.</p>
+        <p>Imbralit, a telha que cobre o Brasil.</p>
+      </div>
+      <a class="video-link fancybox" href="#">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/play-video.svg" alt="Play Video">
+        <p>Assista ao vídeo do produto</p>
+      </a>
+    </div>
+  </section>
+
+</main>
+
+<?php get_footer(); ?>
--- single-produtos.php
+++ single-produtos.php
@@ -2,100 +2,135 @@
 
 <main id="single-products">
     <?php if (have_posts()):
-    while (have_posts()):
-      the_post(); ?>
+        while (have_posts()):
+            the_post(); ?>
 
-    <section class="intro">
-        <div class="container">
-            <div class="content">
-                <h3>Página inicial > Produtos > telhas > <span><?php the_title(); ?></span></h3>
-                <h1><?php the_title(); ?></h1>
-                <h2>Referência: <?php echo get_post_meta(get_the_ID(), 'referencia', true); ?></h2>
-            </div>
-        </div>
-    </section>
-
-    <section class="product-details">
-        <div class="img">
-            <?php if (has_post_thumbnail())
-            the_post_thumbnail('large'); ?>
-        </div>
-        <div class="bottom">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/rotacionar.svg"
-                alt="Arraste para rotacionar">
-            <h2>Arraste para rotacionar</h2>
-        </div>
-    </section>
-
-    <section class="video-product">
-        <a class="video-link fancybox" href="<?php echo get_post_meta(get_the_ID(), 'link_do_video', true); ?>">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/play-video.svg" alt="Play Video">
-            <p>Assista ao vídeo do produto</p>
-        </a>
-    </section>
-
-    <section class="tabs-section">
-        <div class="container">
-            <div class="tabs">
-                <button v-for="tab in tabs" :key="tab.id" :class="{ active: activeTab === tab.id }"
-                    @click="activeTab = tab.id">
-                    {{ tab.label }}
-                </button>
-            </div>
-
-            <div class="tab-content">
-                <div v-if="activeTab === 'descricao'" class="description">
-                    <?php the_content(); ?>
+            <section class="intro">
+                <div class="container">
+                    <div class="content">
+                        <h3>Página inicial > Produtos > telhas > <span><?php the_title(); ?></span></h3>
+                        <h1><?php the_title(); ?></h1>
+                        <h2>Referência: <?php echo get_post_meta(get_the_ID(), 'referencia', true); ?></h2>
+                    </div>
                 </div>
-                <div v-if="activeTab === 'especificacoes'">
-                    <?php echo get_post_meta(get_the_ID(), 'especificacoes_tecnicas', true); ?>
+            </section>
+
+            <section class="product-details">
+                <div class="img">
+                    <?php if (has_post_thumbnail())
+                        the_post_thumbnail('large'); ?>
                 </div>
-                <div v-if="activeTab === 'downloads'">
-                    <h3>Downloads</h3>
-                    <div><?php echo get_post_meta(get_the_ID(), 'avaliacoes', true); ?></div>
+                <div class="bottom">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/rotacionar.svg"
+                        alt="Arraste para rotacionar">
+                    <h2>Arraste para rotacionar</h2>
                 </div>
-            </div>
-        </div>
-    </section>
+            </section>
 
-    <?php
-      $current_product_id = get_the_ID();
+            <?php
+            $link_do_video = get_post_meta(get_the_ID(), 'link_do_video', true);
 
-      $args = array(
-        'post_type' => 'produtos',
-        'posts_per_page' => 4,
-        'post__not_in' => array($current_product_id),
-      );
+            if ($link_do_video): ?>
+                <section class="video-product">
+                    <a class="video-link fancybox" href="<?php echo esc_url($link_do_video); ?>">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/play-video.svg" alt="Play Video">
+                        <p>Assista ao vídeo do produto</p>
+                    </a>
+                </section>
+            <?php endif; ?>
 
-      $related_products = new WP_Query($args);
-      ?>
+            <section class="tabs-section">
+                <div class="container">
+                    <div class="tabs">
+                        <button v-for="tab in tabs" :key="tab.id" :class="{ active: activeTab === tab.id }"
+                            @click="activeTab = tab.id">
+                            {{ tab.label }}
+                        </button>
+                    </div>
+                    <div class="tab-content">
+                        <div v-if="activeTab === 'descricao'" class="description">
+                            <?php the_content(); ?>
+                        </div>
+                        <div v-if="activeTab === 'especificacoes'">
+                            <?php
+                            $especificacoes = get_post_meta(get_the_ID(), 'especificacoes_tecnicas', true);
 
-    <section class="products-catalog">
-        <div class="container">
-            <h2>Confira outros produtos</h2>
-            <div class="list-products">
-                <?php if ($related_products->have_posts()): ?>
-                <?php while ($related_products->have_posts()):
-                $related_products->the_post(); ?>
-                <a href="<?php the_permalink(); ?>" class="item-product">
-                    <div class="img">
-                        <?php if (has_post_thumbnail()): ?>
-                        <?php the_post_thumbnail('medium'); ?>
+                            if ($especificacoes):
+                                echo do_shortcode($especificacoes);
+                            else:
+                                echo '<p>Não há especificações técnicas disponíveis para este produto.</p>';
+                            endif;
+                            ?>
+                        </div>
+                        <div v-if="activeTab === 'downloads'">
+                            <h3>Downloads</h3>
+                            <?php
+                            if (have_rows('downloads')): ?>
+                                <div class="downloads-list">
+                                    <?php
+                                    while (have_rows('downloads')):
+                                        the_row();
+                                        $titulo_do_arquivo = get_sub_field('titulo_do_arquivo');
+                                        $upload_do_arquivo = get_sub_field('upload_do_arquivo');
+
+                                        if ($titulo_do_arquivo && $upload_do_arquivo):
+                                            ?>
+                                            <a href="<?php echo esc_url($upload_do_arquivo['url']); ?>" download>
+                                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/solar_download-outline.svg"
+                                                    alt="Download arquivo">
+                                                <p><?php echo esc_html($titulo_do_arquivo); ?></p>
+                                            </a>
+                                            <br>
+                                        <?php endif; ?>
+                                    <?php endwhile; ?>
+                                </div>
+                            <?php else: ?>
+                                <p>Não há arquivos para download.</p>
+                            <?php endif; ?>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+            <?php
+            $current_product_id = get_the_ID();
+
+            $args = array(
+                'post_type' => 'produtos',
+                'posts_per_page' => 4,
+                'post__not_in' => array($current_product_id),
+            );
+
+            $related_products = new WP_Query($args);
+            ?>
+
+            <section class="products-catalog">
+                <div class="container">
+                    <h2>Confira outros produtos</h2>
+                    <div class="list-products">
+                        <?php if ($related_products->have_posts()): ?>
+                            <?php while ($related_products->have_posts()):
+                                $related_products->the_post(); ?>
+                                <a href="<?php the_permalink(); ?>" class="item-product">
+                                    <div class="img">
+                                        <?php if (has_post_thumbnail()): ?>
+                                            <?php the_post_thumbnail('medium'); ?>
+                                        <?php endif; ?>
+                                    </div>
+                                    <h3><?php the_title(); ?></h3>
+                                    <span class="btn tertiary">ver mais</span>
+                                </a>
+                            <?php endwhile; ?>
+                            <?php wp_reset_postdata(); ?>
+                        <?php else: ?>
+                            <p>Nenhum outro produto encontrado.</p>
                         <?php endif; ?>
                     </div>
-                    <h3><?php the_title(); ?></h3>
-                    <span class="btn tertiary">ver mais</span>
-                </a>
-                <?php endwhile; ?>
-                <?php wp_reset_postdata(); ?>
-                <?php else: ?>
-                <p>Nenhum outro produto encontrado.</p>
-                <?php endif; ?>
-            </div>
-        </div>
-    </section>
+                </div>
+            </section>
 
-    <?php endwhile; endif; ?>
+        <?php endwhile; endif; ?>
 </main>
 
 <?php get_footer(); ?>
