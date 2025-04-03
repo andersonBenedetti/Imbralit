587289b1fb4bdf4187ba4de943bba7a471162090
Anderson Benedetti <andersonjcbenedetti@gmail.com>
2025-04-03 15:49:35 -0300

    feat: add page buy here

---
+++ page-buy-here.php
@@ -0,0 +1,52 @@
+<?php // Template Name: Compre aqui ?>
+
+<?php get_header(); ?>
+
+<main id="pg-buy-here">
+
+    <section class="intro">
+        <div class="container">
+            <h1>Escolha o caminho certo para comprar Imbralit</h1>
+            <p>Seja você uma empresa parceira, um novo comprador ou um consumidor final, aqui você encontra o canal
+                ideal
+                para sua necessidade. Escolha abaixo a melhor opção e siga para o próximo passo.</p>
+        </div>
+    </section>
+
+    <section class="section-content">
+        <div class="container">
+            <div class="items-content">
+                <div class="item-content">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home-center.webp"
+                        alt="Construtora, Home Center ou Material de Construção?">
+                    <div>
+                        <h3>Construtora, Home Center ou Material de Construção?</h3>
+                        <p>Se você deseja comprar e ainda não tem cadastro, nossa equipe te ajuda em todo processo.</p>
+                    </div>
+                    <a class="btn tertiary" href="/fale-conosco">entre em contato</a>
+                </div>
+
+                <div class="item-content">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/quero-comprar.webp"
+                        alt="Não tenho CNPJ, quero comprar perto de mim">
+                    <div>
+                        <h3>Não tenho CNPJ, quero comprar perto de mim</h3>
+                        <p>Veja onde encontrar produtos Imbralit em lojas autorizadas perto de você.</p>
+                    </div>
+                    <a class="btn tertiary" href="/onde-encontrar">Encontrar lojas</a>
+                </div>
+
+                <div class="item-content">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/sou-cliente.webp"
+                        alt="Já sou cliente Imbralit">
+                    <div>
+                        <h3>Já sou cliente Imbralit</h3>
+                        <p>Faça pedidos, consulte condições e tenha acesso exclusivo ao nosso sistema.</p>
+                    </div>
+                    <a class="btn tertiary" href="/onde-encontrar">acessar b2b</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</main>
